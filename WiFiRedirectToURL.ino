5af76c2d2882266c69d26ebc183bf2a3a06e1d7a
konstie <145743301+konstie1@users.noreply.github.com>
2023-12-27 16:44:27 +0200

    Update WiFiRedirectToURL.ino

--- WiFiRedirectToURL.ino
+++ WiFiRedirectToURL.ino
@@ -10,28 +10,59 @@ ESP8266WebServer webServer(80);
 // Пример HTML-шаблона
 const char* htmlTemplate = R"(
 <!DOCTYPE html>
-<html>
+<html lang="uk">
 <head>
-  <title>Перенаправление</title>
+  <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <script>
-    // Простой JavaScript счетчик посещений
-    document.addEventListener('DOMContentLoaded', function() {
-      var counterElement = document.getElementById('counter');
-      var currentCount = parseInt(counterElement.innerText);
-      currentCount++;
-      counterElement.innerText = currentCount;
-    });
-  </script>
+  <title>Ласкаво просимо!</title>
+  <style>
+    body {
+      font-family: 'Arial', sans-serif;
+      background-color: #f4f4f4;
+      color: #333;
+      margin: 20px;
+      text-align: center;
+    }
+
+    h1 {
+      color: #0066cc;
+    }
+
+    p {
+      margin-bottom: 15px;
+    }
+
+    a {
+      color: #0066cc;
+      text-decoration: none;
+      font-weight: bold;
+    }
+
+    a:hover {
+      text-decoration: underline;
+    }
+
+    .container {
+      max-width: 600px;
+      margin: 0 auto;
+      padding: 20px;
+      background-color: #fff;
+      border-radius: 8px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+  </style>
 </head>
 <body>
-  <h1>Вы были перенаправлены!</h1>
-  <p>Это простая перенаправляющая страница.</p>
-  <p>Автор: <a href="https://www.buymeacoffee.com/konstie">https://www.buymeacoffee.com/konstie</a></p>
-  <p>Информация о проекте: Этот проект создан для отслеживания количества людей, подключающихся к небезопасной сети. Чтобы узнать больше о том, как работает проект, и получить доступ к статистике, рекомендуем подписаться <a href="https://www.buymeacoffee.com/konstie">здесь</a>.</p>
-  <p>Счетчик посещений: <span id="counter">0</span></p>
+  <div class="container">
+    <h1>Ласкаво просимо!</h1>
+    <p>Ця сторінка створена в межах проекту, спрямованого на підвищення обізнаності щодо проблем інформаційної безпеки.</p>
+    <p>Наша мета - привернути увагу до питань безпеки в мережі та створити статистику, яка відображатиме підключення до небезпечних мереж.</p>
+    <p>Якщо ви хочете дізнатися більше про зібрану <b>статистику</b> або бажаєте <b>підтримати автора</b>, ми запрошуємо вас відвідати наступну сторінку:</p>
+    <p><a href="https://www.buymeacoffee.com/konstie">Підтримати автора</a></p>
+  </div>
 </body>
 </html>
+
 )";
 
 void setup() {
